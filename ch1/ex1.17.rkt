c420f17d170066311f24c8cdef2b3f930f289d31
Dwayne Crooks <me@dwaynecrooks.com>
2020-10-08 05:41:36 -0400

    Add solutions to exercises 1.15 to 1.30

---
+++ ch1/ex1.17.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (down lst)
+  (map list lst))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal?
+   (down '(1 2 3))
+   '((1) (2) (3)))
+
+  (check-equal?
+   (down '((a) (fine) (idea)))
+   '(((a)) ((fine)) ((idea))))
+
+  (check-equal?
+   (down '(a (more (complicated)) object))
+   '((a) ((more (complicated))) (object))))
---
+++ ch1/ex1.26.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (up lst)
+  (if (null? lst)
+      '()
+      ((if (list? (car lst)) append cons)
+       (car lst)
+       (up (cdr lst)))))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal?
+   (up '((1 2) (3 4)))
+   '(1 2 3 4))
+
+  (check-equal?
+   (up '((x (y)) z))
+   '(x (y) z)))
